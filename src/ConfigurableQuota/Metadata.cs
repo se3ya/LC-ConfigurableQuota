7d8a92646a101046c858a2a2dd3a161eeba04aab
seeya <116365392+se3ya@users.noreply.github.com>
2025-11-12 21:19:32 +0200

    .

--- src/ConfigurableQuota/Metadata.cs
+++
@@ -1,9 +0,0 @@
-namespace ConfigurableQuota
-{
-    internal static class Metadata
-    {
-        public const string GUID = "com.seeya.configurablequota";
-        public const string PLUGIN_NAME = "Configurable Quota";
-        public const string VERSION = "1.0.0";
-    }
-}
