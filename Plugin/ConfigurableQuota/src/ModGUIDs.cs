cf9c5ad6d5336d887f5dcad8dd21ca8b0b2b0b86
seeya <seeyagm@proton.me>
2026-08-11 20:43:29 +0300

    chore: add LGU and ScrapInsurance guids

--- Plugin/ConfigurableQuota/src/ModGUIDs.cs
+++ Plugin/ConfigurableQuota/src/ModGUIDs.cs
@@ -9,5 +9,7 @@ namespace ConfigurableQuota
         public const string LLL_GUID = "imabatby.lethallevelloader";
         public const string LUNAR_CONFIG_GUID = "Crafty.LunarConfig";
         public const string SELF_SORTING_STORAGE_GUID = "zigzag.SelfSortingStorage";
+        public const string MORE_SHIP_UPGRADES_GUID = "com.malco.lethalcompany.moreshipupgrades";
+        public const string SCRAP_INSURANCE_GUID = "com.github.WhiteSpike.ScrapInsurance";
     }
 }
